f2c4f76667b9c15e601b875d8b4594bd5ef23017
Hao Kung <haokung+github@gmail.com>
2022-12-29 12:13:40 -0800

    Add some jwt tests

---
+++ src/Identity/test/Identity.Bearer.Test/JwtBuilderTest.cs
@@ -0,0 +1,105 @@
+// Licensed to the .NET Foundation under one or more agreements.
+// The .NET Foundation licenses this file to you under the MIT license.
+
+namespace Microsoft.AspNetCore.Identity.Test;
+
+public class JwtBuilderTest
+{
+    private JsonWebKey _noneKey = new JsonWebKey(JWSAlg.None);
+
+    [Fact]
+    public async Task CanRetrieveClaims()
+    {
+        var payload = new Dictionary<string, string>();
+        payload["email"] = "email";
+        var jwtBuilder = new JwtBuilder(
+            JWSAlg.None,
+            "issuer",
+            _noneKey,
+            audience: "audience",
+            subject: string.Empty,
+            payload,
+            DateTimeOffset.UtcNow,
+            DateTimeOffset.UtcNow.AddMinutes(30));
+        var token = await jwtBuilder.CreateJwtAsync();
+
+        var jwtReader = new JwtReader(JWSAlg.None, "issuer", _noneKey, "audience");
+        var user = await jwtReader.ValidateJwtAsync(token);
+
+        Assert.NotNull(user);
+        Assert.Contains(user.Claims, c => c.Type == "email" && c.Value == "email");
+    }
+
+    [Fact]
+    public async Task RejectedAfterExpires()
+    {
+        var jwtBuilder = new JwtBuilder(
+            JWSAlg.None,
+            "issuer",
+            _noneKey,
+            audience: "audience",
+            subject: string.Empty, // TODO: combine this with CreatePayload?
+            payload: null,
+            DateTimeOffset.UtcNow,
+            DateTimeOffset.UtcNow.AddMinutes(-30));
+        var token = await jwtBuilder.CreateJwtAsync();
+
+        var jwtReader = new JwtReader(JWSAlg.None, "issuer", _noneKey, "audience");
+        Assert.Null(await jwtReader.ValidateJwtAsync(token));
+    }
+
+    [Fact]
+    public async Task RejectedBeforeNotBefore()
+    {
+        var jwtBuilder = new JwtBuilder(
+            JWSAlg.None,
+            "issuer",
+            _noneKey,
+            audience: "audience",
+            subject: string.Empty,
+            payload: null,
+            DateTimeOffset.UtcNow.AddMinutes(5), // NotBefore set to 5 minutes in the future
+            DateTimeOffset.UtcNow.AddMinutes(30));
+        var token = await jwtBuilder.CreateJwtAsync();
+
+        var jwtReader = new JwtReader(JWSAlg.None, "issuer", _noneKey, "audience");
+        Assert.Null(await jwtReader.ValidateJwtAsync(token));
+    }
+
+    [Fact]
+    public async Task RejectedWrongIssuer()
+    {
+        var jwtBuilder = new JwtBuilder(
+            JWSAlg.None,
+            "badissuer",
+            _noneKey,
+            audience: "audience",
+            subject: string.Empty,
+            payload: null,
+            DateTimeOffset.UtcNow.AddMinutes(5), // NotBefore set to 5 minutes in the future
+            DateTimeOffset.UtcNow.AddMinutes(30));
+        var token = await jwtBuilder.CreateJwtAsync();
+
+        var jwtReader = new JwtReader(JWSAlg.None, "issuer", _noneKey, "audience");
+        Assert.Null(await jwtReader.ValidateJwtAsync(token));
+    }
+
+    [Fact]
+    public async Task RejectedWrongAudience()
+    {
+        var jwtBuilder = new JwtBuilder(
+            JWSAlg.None,
+            "issuer",
+            _noneKey,
+            audience: "badaudience",
+            subject: string.Empty,
+            payload: null,
+            DateTimeOffset.UtcNow.AddMinutes(5), // NotBefore set to 5 minutes in the future
+            DateTimeOffset.UtcNow.AddMinutes(30));
+        var token = await jwtBuilder.CreateJwtAsync();
+
+        var jwtReader = new JwtReader(JWSAlg.None, "issuer", _noneKey, "audience");
+        Assert.Null(await jwtReader.ValidateJwtAsync(token));
+    }
+
+}
